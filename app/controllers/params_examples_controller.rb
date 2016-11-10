71b3fae3816d2296c2bb994b52bdff3a0ea6a035
Peter Jang <peter.x.jang@gmail.com>
2016-11-09 19:52:08 -0600

    Add solution to name exercise

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -4,4 +4,13 @@ class ParamsExamplesController < ApplicationController
     @message2 = params["another_message"]
     render 'query_params.html.erb'
   end
+
+  def whatever_name_method
+    @name = params["query_name"]
+    @message = ""
+    if @name.upcase.starts_with?("A")
+      @message = "Hey your name starts with A!"
+    end
+    render 'name.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/query_params_url' => 'params_examples#query_params_method'
+  get '/name_url' => 'params_examples#whatever_name_method'
 end
